4d01de72848b715502af31db48dfc11657ff741f
David Tielke <mail@david-tielke.de>
2024-06-03 11:27:18 +0200

    Add project files.

---
+++ PersonManagerApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PersonManagerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = File.ReadAllLines("data.csv")
+                .Select(l => l.Split(","))
+                .Select(p => new Person
+                {
+                    Id = int.Parse(p[0]),
+                    Name = p[1],
+                    Age = int.Parse(p[2]),
+                })
+                .ToList();
+
+            var adults = persons.Where(p => p.Age >= 18).ToList();
+            Console.WriteLine($"### ADULTS ({adults.Count}) ###");
+            adults.ForEach(a => Console.WriteLine(a.Name));
+
+
+            var children = persons.Where(p => p.Age < 18).ToList();
+            Console.WriteLine($"### CHILDREN ({children.Count}) ###");
+            children.ForEach(c => Console.WriteLine(c.Name));
+        }
+    }
+}
