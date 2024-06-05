15b63d696d6c43345f2205b286f737a56a7c97f8
David Tielke <mail@david-tielke.de>
2024-06-05 10:19:34 +0200

    - Another Wed morning

--- PersonManagerApp/PersonRepository.cs
+++ PersonManagerApp/PersonRepository.cs
@@ -11,23 +11,12 @@ class PersonRepository
     {
         _reader = new FileReader();
         _parser = new PersonParser();
-
-        var utilities = new PersonUtilities();
     }
 
-    public object Create()
-    {
-        return new object();
-    }
     public IQueryable<Person> Query()
     {
         var csvLines = _reader.ReadAllLines("data.csv");
         var persons = csvLines.Select(line => _parser.Parse(line));
         return persons.AsQueryable();
     }
-}
-
-class PersonUtilities
-{
-    public void Foo(){}
 }
